c27cf49e195f49c9f84659029728fa7d74ff57f1
gim-wan-yeong <dhks2869@gmail.com>
2021-02-07 22:19:16 +0900

    update calender

--- src/calender.java
+++ src/calender.java
@@ -2,9 +2,11 @@ public class calender {
     public static void main(String[] args){
         System.out.println("일 월 화 수 목 금 토");
         System.out.println("----------------");
-        System.out.println("1 2 3 4 5 6 7 8");
-        System.out.println("9 10 11 12 13 14 15");
-        System.out.println("16 17 18 19 20 21 22");
-        System.out.println("23 24 25 26 27 28 29");
+        System.out.println(" 1  2  3  4  5  6  7");
+        System.out.println(" 8  9 10 11 12 13 14");
+        System.out.println("15 16 17 18 19 20 21");
+        System.out.println("22 23 24 25 26 27 28");
+        System.out.println("29 30 31");
+
     }
 }
